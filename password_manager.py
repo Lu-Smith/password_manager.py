e80527376c2f860a25f7c601b088f55299ad6bf4
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-05-11 16:21:33 +0100

    Updated logic in password_manager.py

--- password_manager.py
+++ password_manager.py
@@ -1,25 +1,31 @@
-pwd = input("What is the master password? ")
+pwd = input("What is the master password? ").lower()
 
 def view():
-    pass
+    with open("passwords.txt", "r") as f:
+        for line in f.readlines():
+            print(line)
 
 def add():
     name = input("Account Name: ")
     pwd = input("Password: ")
 
     with open("passwords.txt","a") as f:
-        f.write(name + "|" + pwd)
+        f.write(name + "|" + pwd + "\n")
 
-while True:
-    mode = input("Would you like to add a new password or view existing ones? (Type: Add/View or Q  to quit) ").lower()
 
-    if mode == "q":
-        break
+if pwd == "cat":
+    while True:
+        mode = input("Would you like to add a new password or view existing ones? (Type: Add/View or Q  to quit) ").lower()
 
-    if mode == "view": 
-        view()
-    elif mode == "add":
-        add()
-    else: 
-        print("Invalid mode.")
-        continue
+        if mode == "q":
+            break
+
+        if mode == "view": 
+            view()
+        elif mode == "add":
+            add()
+        else: 
+            print("Invalid mode.")
+            continue
+else:
+    print("Wrong manster password, try again.")
