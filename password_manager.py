91c932123392dd468330a5d9dbcc1017f1564240
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-05-11 16:14:03 +0100

    Created passwords.txt in password_manager.py

--- password_manager.py
+++ password_manager.py
@@ -7,6 +7,9 @@ def add():
     name = input("Account Name: ")
     pwd = input("Password: ")
 
+    with open("passwords.txt","a") as f:
+        f.write(name + "|" + pwd)
+
 while True:
     mode = input("Would you like to add a new password or view existing ones? (Type: Add/View or Q  to quit) ").lower()
 
